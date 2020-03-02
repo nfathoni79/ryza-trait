aaac2e056be2c8f0ef0cc506be110d1a27fc8353
NN Fathoni <31845226+nfathoni79@users.noreply.github.com>
2020-03-02 11:39:00 +0700

    Use secure asset

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <title>{{ $title }}</title>
 
-    <link rel="icon" href="{!! asset('puni-jelly.png') !!}">
-    <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
-    <script src="{!! asset('js/app.js') !!}" charset="utf-8"></script>
+    <link rel="icon" href="{!! secure_asset('puni-jelly.png') !!}">
+    <link rel="stylesheet" href="{!! secure_asset('css/app.css') !!}">
+    <script src="{!! secure_asset('js/app.js') !!}" charset="utf-8"></script>
 
     <style>
         body {
             padding-top: 70px;
-            background: url("{!! asset('ryza-wallpaper.jpg') !!}") no-repeat center center fixed;
+            background: url("{!! secure_asset('ryza-wallpaper.jpg') !!}") no-repeat center center fixed;
             -webkit-background-size: cover;
             -moz-background-size: cover;
             background-size: cover;
